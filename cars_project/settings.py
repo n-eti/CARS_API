83ea4dd0623edbbd6d806687ab1cfeaa4bc618c3
Nada Atieh <nada.s.atieh@gmail.com>
2023-01-06 08:24:17 -0600

    Attempted to create first app however it is not connecting to the databbase

--- cars_project/settings.py
+++ cars_project/settings.py
@@ -33,7 +33,9 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
+    'django.contrib.staticfiles', 
+    'rest_framework',
+    'cars',
 ]
 
 MIDDLEWARE = [
